75f48c49fae3980fc95071f9290109ae3c04b7d4
arpitSatpute <arpitrameshsatpute6986@gmail.com>
2025-10-23 18:55:09 +0530

    ADD abi

--- Frontend/src/pages/pricing.tsx
+++ Frontend/src/pages/pricing.tsx
@@ -1,13 +1,17 @@
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import { InteractiveHoverButton } from "@/components/ui/interactive-hover-button";
 
 export default function DocsPage() {
+
+  function handleAirdrop() {
+    alert("Airdrop button clicked!");
+  }
+
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Pricing</h1>
-        </div>
+        <InteractiveHoverButton text="Airdrop" onClick={handleAirdrop}/>
       </section>
     </DefaultLayout>
   );
